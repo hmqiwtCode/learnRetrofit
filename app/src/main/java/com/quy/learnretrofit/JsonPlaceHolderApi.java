88df03e84bd716f690da5254ec3bdb324efbcfcd
Quy <huynhminhquy1999@gmail.com>
2020-09-14 10:11:38 +0700

    Add more

--- app/src/main/java/com/quy/learnretrofit/JsonPlaceHolderApi.java
+++ app/src/main/java/com/quy/learnretrofit/JsonPlaceHolderApi.java
@@ -4,9 +4,31 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface JsonPlaceHolderApi {
 
     @GET("posts")
     Call<List<Post>> getPosts();
+    // Neu endpoint https://jsonplaceholder.typicode.com/posts?userId=1 ->
+    //@GET("posts")
+    // Call<List<Post>> getPosts(@Query("userId") int userID);
+
+    // Neu co nhieu param
+    //@GET("posts")
+    //  Call<List<Post>> getPosts(@Query("userId") int userID, @Query("_sort") String id);
+
+    // Neu co nhieu userId can lay
+    //@GET("posts")
+    //  Call<List<Post>> getPosts(@Query("userId") Integer[] userId, @Query("_sort") String id);
+    // Call<List<Post>> getPosts(@Query("userId") List<Integer> userId, @Query("_sort") String id);
+
+    // Neu muon de null de request All
+    //@GET("posts")
+    //  Call<List<Post>> getPosts(@Query("userId") Integer userId, @Query("_sort") String id);
+
+
+    @GET("posts/{id}/comments")
+    Call<List<Comment>> getComments(@Path("id") int postId);
 }
--- app/src/main/java/com/quy/learnretrofit/MainActivity.java
+++ app/src/main/java/com/quy/learnretrofit/MainActivity.java
@@ -15,32 +15,58 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     TextView tv_display;
+    JsonPlaceHolderApi api;
+    Retrofit retrofit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv_display = findViewById(R.id.tv_display);
-
-        Retrofit retrofit = new Retrofit.Builder()
+        retrofit = new Retrofit.Builder()
                 .baseUrl("https://jsonplaceholder.typicode.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
+        api = retrofit.create(JsonPlaceHolderApi.class);
 
-        JsonPlaceHolderApi api = retrofit.create(JsonPlaceHolderApi.class);
+       // getPost();
+
+        getComment(3);
+
+    }
+
+    private void getPost(){
         Call<List<Post>> call = api.getPosts();
         call.enqueue(new Callback<List<Post>>() {
             @Override
             public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
                 for (Post post : response.body()){
                     String content = "";
-                    content += "ID: " + post.getTitle() + "\n\n\n";
+                    content += "Body: " + post.getTitle() + "\n\n\n";
                     tv_display.append(content);
                 }
+            }
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
 
+            }
+        });
+    }
+
+    private void getComment(int idComment){
+        Call<List<Comment>> comment = api.getComments(idComment);
+        comment.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+                for (Comment comment : response.body()){
+                    String content = "";
+                    content += "CommentId: " + comment.getUserId() + "\n\n\n";
+                    content += "ID: " + comment.getBody() + "\n\n\n";
+                    tv_display.append(content);
+                }
             }
 
             @Override
-            public void onFailure(Call<List<Post>> call, Throwable t) {
+            public void onFailure(Call<List<Comment>> call, Throwable t) {
 
             }
         });
